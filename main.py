af8e18f5a8a3e576ec2569387ce36a8b8176852e
aura <aura@pop-os.localdomain>
2023-05-17 22:23:28 +0530

    some console design changes

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import requests
 import asyncio
 import json
+import sys
+import argparse
+from rich.progress import track
+from rich.console import Console
 
+console = Console()
+
+
+# command line arguments reading
+
+parser = argparse.ArgumentParser(prog="Naukri JBOT")
+
+parser.add_argument("-k", help="input the jobs you want to apply for comma (,) seperated (required)", metavar="[keywords]", dest="keyword", required=True )
+parser.add_argument("-e", metavar='[experience]', dest="experience", help="Job Experience in numbers (optional)", required=False)
+parser.add_argument("-l", metavar='[location]', dest="location", help="Location of the job, (use comma seperated values for multiple locations) (optional)", required=False)
+parser.add_argument("-n", metavar='[no of applies]', dest="no_of_applies", help="Total amount of job applies you want to send, default is 10 (optional)", default=10)
+parser.add_argument("-u", metavar='[username/email]', dest="username", help="Your naukri account's username/ email", required=True)
+parser.add_argument("-p", metavar='[password]', dest="password", help="Your naukri account's password", required=True)
+
+args = parser.parse_args()
 
 # global variables
 host = "https://www.naukri.com"
@@ -21,10 +40,17 @@ async def userAuthentication(username, password):
     }
 
     response = requests.post(url=f'{host}{loginEndpoint}', json=data, headers=headers)
+    responseDict = json.loads(response.text)
+    try:
+        responseDict['cookies']
+    except:
+        console.print(f'[bold red]{responseDict["message"]}')
+        sys.exit()
 
+    console.print(f'[green]Naukri Auth Successful!')
     return response.cookies
 
-async def searchJobs(noOfResults, keyword, location="", experience="", pageNo=1):
+async def searchJobs(keyword, location="", experience="", pageNo=1, noOfResults=10):
     jobSearchEndpoint = "/jobapi/v3/search"
 
     params = {
@@ -35,7 +61,6 @@ async def searchJobs(noOfResults, keyword, location="", experience="", pageNo=1)
         "location": location,
         "experience": experience,
         "pageNo": pageNo,
-        # "sort": "p"
     }
 
     headers = {
@@ -59,19 +84,24 @@ async def applyJobs(jobId, cookies):
     return response.text
 
 async def main():
-    authCookies = await userAuthentication("pranavsayshii@gmail.com", "my_password")
-    jobsData = await searchJobs(pageNo=3, noOfResults=10, keyword="penetration tester, developer", location="hyderabad, bangalore", experience=2)
+    successfulApplies = 0
+    failedApplies = 0
+    authCookies = await userAuthentication(args.username, args.password)
+    jobsData = await searchJobs(noOfResults=args.no_of_applies, keyword=args.keyword, location=args.location, experience=args.experience)
     jobsDataDict = json.loads(jobsData)
     jobDetails = jobsDataDict['jobDetails']
     
-    for jobObj in jobDetails:
+    for jobObj in track(jobDetails, description="[bold cyan]Jobs applying ..."):
         jobApplyStatus = await applyJobs(jobId=jobObj['jobId'], cookies=authCookies)
         jobApplyStatusDict = json.loads(jobApplyStatus)
+        console.print(f'[cyan] applying for {jobObj["title"]}')
         try:
             isJobAppliedSuccessfully = jobApplyStatusDict['redirectURL']
-            print('job success === ', isJobAppliedSuccessfully)
+            successfulApplies = successfulApplies + 1
         except:
-            print('job fail')
+            failedApplies = failedApplies + 1
+
+    console.print(f'[green]Total Job Applies: {args.no_of_applies}\nSuccessful Job Applies: {successfulApplies}\nFailed Job Applies: {failedApplies}\n[cyan]Note: Failed applies are jobs which needs to be applied outside naukri website.')
 
 
 
